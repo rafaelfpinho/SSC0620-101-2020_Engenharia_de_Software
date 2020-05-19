d27aa4c9a7eb2d320e183bf79e5884c0ffe3ec69
rafaelfpinho <rafaelfpinho@usp.br>
2020-05-19 16:26:12 -0300

    Add files via upload

---
+++ NotaFinal/Disciplina.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Disciplina {
+    private String codigo;
+    ArrayList<String> estudantes = new ArrayList();
+
+    //getters and setters
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+
+}
---
+++ NotaFinal/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    
+	Disciplina disciplina = new Disciplina();
+	Aluno aluno = new Aluno();
+
+}
