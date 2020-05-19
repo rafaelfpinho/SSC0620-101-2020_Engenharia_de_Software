a17ca6db8dc18521616b472816b8c72e93225f03
rafaelfpinho <rafaelfpinho@usp.br>
2020-05-19 16:30:41 -0300

    Add files via upload

--- NotaFinal/Aluno.java
+++ NotaFinal/Aluno.java
@@ -1,3 +1,40 @@
 public class Aluno {
-    dsfaliueewbfaçewibweoifwa;
+    private String nome;
+    private int n_usp;
+    private float notas;
+    private float media_final;
+
+    //getters and setters
+    public float getNotas() {
+        return notas;
+    }
+
+    public float getMedia_final() {
+        return media_final;
+    }
+
+    public void setMedia_final(float media_final) {
+        this.media_final = media_final;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getN_usp() {
+        return n_usp;
+    }
+
+    public void setN_usp(int n_usp) {
+        this.n_usp = n_usp;
+    }
+
+    public void setNotas(float notas) {
+        this.notas = notas;
+    }
+
 }
