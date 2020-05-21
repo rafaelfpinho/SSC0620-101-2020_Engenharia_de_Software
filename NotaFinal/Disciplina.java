8a59a2506b0da7faee5b322cac7c3490243455a9
rafaelfpinho <rafaelfpinho@usp.br>
2020-05-20 23:58:57 -0300

    Add files via upload

--- NotaFinal/Disciplina.java
+++ NotaFinal/Disciplina.java
@@ -7,51 +7,59 @@ import java.util.Iterator;
 public class Disciplina{
 	
 	private String codigo;
-	private ArrayList<Aluno>estudantes;
+    private ArrayList<Aluno>estudantes;
+    String saidaval = "ID válido";
+    String saidainval = "ID inválido";
 
 	public Disciplina(String codigo){
         setCodigo(codigo);
         estudantes = new ArrayList<Aluno>();
-}
+    }
 
-public boolean setCodigo (String codigo) {
-this.codigo = codigo;
-return true;
-}
+
+    public boolean setCodigo (String codigo) {
+        if(codigo.length() == 7 && codigo.substring(0, 3).matches("SSC") && codigo.substring(3, 7).matches("[0-9]*")){
+            this.codigo = codigo;
+            return true;
+        }
+        else 
+            return false;        
+    }
 
     public boolean adicionarEstudantes(Aluno aluno){
-	    estudantes.add(aluno);
-	    return true;
+        if(estudantes.add(aluno)){
+            return true;
+        }
+        return false;
     }
-	
+    
     public int alunosMatriculados( ) {
-	    return estudantes.size( );
+        return estudantes.size( );
     }
 
-public void imprimirEstudantesDecrescente( ){
-    Iterator<Aluno> it1 = estudantes.iterator();
-    while (it1.hasNext()) {
-        it1.next().setFlag(1);    
+    public void imprimirEstudantesDecrescente( ){
+        Iterator<Aluno> it1 = estudantes.iterator();
+        while (it1.hasNext()) {
+            it1.next().setFlag(1);    
+        }
+
+        Collections.sort(estudantes, Collections.reverseOrder());
+        Iterator<Aluno> it = estudantes.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next().getNusp());
+        }
     }
-Collections.sort(estudantes, Collections.reverseOrder());
-
-Iterator<Aluno> it = estudantes.iterator();
-while (it.hasNext()) {
-    System.out.println(it.next().getNusp());
-}
-}
-
 
     public void imprimirEstudantesMedias(){
-    Iterator<Aluno> it1 = estudantes.iterator();
-    while (it1.hasNext()) {
-        it1.next().setFlag(0);    
-    }
-    Collections.sort(estudantes, Collections.reverseOrder());
-    Iterator<Aluno> it = estudantes.iterator();
-    while (it.hasNext()) {
-        System.out.println(it.next().getMediaFinal());
-    }
+        Iterator<Aluno> it1 = estudantes.iterator();
+        while (it1.hasNext()) {
+            it1.next().setFlag(0);    
+        }
+        Collections.sort(estudantes, Collections.reverseOrder());
+        Iterator<Aluno> it = estudantes.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next().getMediaFinal());
+        }
     }
 
     public void imprimirAprovados(){
@@ -61,11 +69,43 @@ while (it.hasNext()) {
             if(x.getMediaFinal() >= 5.0){
                 System.out.print(x.getNusp()+" ");
                 System.out.println(x.getMediaFinal());
-                //System.out.print(it.next().getNotas());
+            }
+        }
+    }
+
+    public int aprovados(){
+        int quantidade = 0;
+        Iterator<Aluno> it = estudantes.iterator();
+        while (it.hasNext()) {
+            Aluno x = it.next();
+            if(x.getMediaFinal() >= 5.0){
+                quantidade++;
+            }
+        }
+        return quantidade;
+    }
+    
+    public void imprimirReprovados(){
+        Iterator<Aluno> it = estudantes.iterator();
+            while (it.hasNext()) {
+                Aluno x = it.next();
+                if(x.getMediaFinal() < 5.0){
+                    System.out.print(x.getNusp()+" ");
+                    System.out.println(x.getMediaFinal());
+                }
             }
         }
 
+        public int reprovados(){
+            int quantidade = 0;
+            Iterator<Aluno> it = estudantes.iterator();
+            while (it.hasNext()) {
+                Aluno x = it.next();
+                if(x.getMediaFinal() < 5.0){
+                    quantidade++;
+                }
+            }
+            return quantidade;
         }
 
-    }
-
+}
